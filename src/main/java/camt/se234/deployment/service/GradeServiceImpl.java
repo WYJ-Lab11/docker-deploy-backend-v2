d1c7a7aa5b4e9281cc3e31088218e411e86894dc
Yvette <1402738213@qq.com>
2026-06-07 00:32:19 +0800

    Update grade calculation criteria

--- src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
@@ -5,19 +5,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GradeServiceImpl implements GradeService {
+
     @Override
     public String getGrade(double score) {
-        if (score > 79.5) {
+        // add a new rule
+        if (score > 90.0) {
+            return "A+";
+        } else if (score > 80.0) {
             return "A";
-        }
-        else if (score > 74.5){
+        } else if (score > 70.0) {
             return "B";
-        }else if (score > 59.5){
+        } else if (score > 60.0) {
             return "C";
-        }else if (score > 32.5) {
-            return "D";
-        } else
+        } else {
             return "F";
-
+        }
     }
 }
